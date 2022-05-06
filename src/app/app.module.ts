61383402ee8049c7e4a87ce17400ab9bc0cb8ecb
sanjana2601 <ssamale2601@gmail.com>
2022-05-06 08:57:24 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { BodyComponentComponent } from './body-component/body-component.component';
+import { TodoComponentComponent } from './body-component/todo-component/todo-component.component';
+import { InprogressComponentComponent } from './body-component/inprogress-component/inprogress-component.component';
+import { DoneComponentComponent } from './body-component/done-component/done-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    BodyComponentComponent,
+    TodoComponentComponent,
+    InprogressComponentComponent,
+    DoneComponentComponent
   ],
   imports: [
     BrowserModule
